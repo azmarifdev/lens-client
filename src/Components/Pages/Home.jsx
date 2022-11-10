e94e780f0c33cdaa002b3779cea2e86aed200d76
A. Z. M. Arif <azmarifcode@gmail.com>
2022-11-10 21:14:30 +0600

    toast add

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
                 }
             })
             .catch((err) => {
-                toast.error(err.message);
+                toast.error('something went wrong');
             });
     }, []);
 
